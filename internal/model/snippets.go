ee8e5229f3f28d4b3290fb19ab68f3085d81a239
Antonio Mariani <antonio.mariani@cmcc.it>
2024-11-16 17:01:54 +0100

    introducing snippet.go

---
+++ internal/model/snippets.go
@@ -0,0 +1,31 @@
+package model
+
+import (
+	"database/sql"
+	"time"
+)
+
+type Snippet struct {
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
+}
+
+// SnippetModel wrapping a sql.DB connections
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	return 0, nil
+}
+
+func (m *SnippetModel) Get(id int) (*Snippet, error) {
+	return nil, nil
+}
+
+func (m *SnippetModel) latest() ([]*Snippet, error) {
+	return nil, nil
+}
